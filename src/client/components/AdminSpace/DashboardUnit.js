9211ca38c04c6161730c50d06df5cca9c2a00046
radhoin ben haj bel kacem <radhoinbenhk@gmail.com>
2018-02-07 19:31:59 +0100

    Dashboard style 1

--- src/client/components/AdminSpace/DashboardUnit.js
+++ src/client/components/AdminSpace/DashboardUnit.js
@@ -4,8 +4,12 @@ import { withStyles } from 'material-ui/styles';
 import QueryBuilder from 'material-ui-icons/QueryBuilder';
 import People from 'material-ui-icons/People'
 import Button from 'material-ui/Button';
+import Typography from 'material-ui/Typography';
 import moment from 'moment'
 const styles = theme => ({
+  ChartContainer:{
+    width:'100%',
+  },
   card: {
     backgroundColor : '#053787',
     color :'white'
@@ -53,20 +57,44 @@ const styles = theme => ({
     width : '50%' ,
     float : 'right'
   } ,
+  workshopName:{
+    fontSize: '1.5rem'
+  },
+  timeDetail:{
+    display: 'flex',
+    paddingLeft: '10px',
+    flexDirection: 'column',
+    borderLeft: '2px solid #8080805c',
+    marginLeft: '10px',
+  },
+  timeDetailText:{
+    color: 'gray',
+    marginBottom: '5px',
+  },
+  timeDetailHour:{
+    fontSize: '21pt',
+    fontweight: 'bold',
+  }
 
 });
 const containers={
   DetailContainer:{
     display: 'flex',
-    flexDirection: 'row'
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   ChartContainer:{
 
   },
-  smallFont :{
-    fontSize: 'small',
-    width : '100%' ,
-    float : 'left'
+  containerDetail :{
+    // fontSize: 'small',
+    // width : '100%' ,
+    // float : 'left'
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: '10px',
   }
 }
 class DashboardUnit extends React.Component{
@@ -90,44 +118,51 @@ class DashboardUnit extends React.Component{
         return(
           <div style={containers.DetailContainer} key={details._id}>
             <div className="ChartContainer">
-              <PieChart width={400} height={400}>
-              <Pie data={data} cx="50%" cy="50%" innerRadius={74} outerRadius={80} fill="#00ABC7" label >
-                {
-                  data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
-                }
-                <Label value="Running Time" position="topcenter" className={classes.boldLabel}/>
-                <Label value={`${difference._data.hours}h${difference._data.minutes}mn`} position="center" className={classes.raffined} />
-              </Pie>
-              <Pie data={data} cx="50%" cy="50%" innerRadius={45} outerRadius={60} fill="#00abc7" >
-                {
-                  data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
-                }
-              </Pie>
+                <PieChart width={400} height={400}>
+                  <Pie data={data} cx="50%" cy="50%" innerRadius={74} outerRadius={80} fill="#00ABC7" label >
+                    {
+                      data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
+                    }
+                    <Label value="Running Time" position="topcenter" className={classes.boldLabel}/>
+                    <Label value={`${difference._data.hours}h${difference._data.minutes}mn`} position="center" className={classes.raffined} />
+                  </Pie>
+                  <Pie data={data} cx="50%" cy="50%" innerRadius={45} outerRadius={60} fill="#00abc7" >
+                    {
+                      data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
+                    }
+                  </Pie>
 
-              <Tooltip/>
-            </PieChart>
-          </div>
-                <div><h3>Workshop Name</h3>
-                <div style={containers.smallFont}>
+                  <Tooltip/>
+                </PieChart>
+              </div>
+              <div className={classes.ChartContainer}>
+                <Typography variant="headline" gutterBottom className={classes.workshopName}>
+                  Workshop Name
+                </Typography>
+                <div style={containers.containerDetail}>
                   <Button fab disabled><QueryBuilder color="action"/>
                   </Button>
-                  <span >Started At </span>
-                  <span>{moment(details.start_hour).utcOffset(1, true).format('hh:mm')}</span>
-                </div>
-                {(details.end_hour!=null)&&(<div style={containers.smallFont}>
-
-                    <span>Ended At </span>
-                    <span>{moment(details.end_hour).utcOffset(1, true).format('hh:mm')}</span>
-                </div>)}
-                <div>
-                  <div  style={containers.smallFont}><Button fab disabled>
-                    <People color="action"/>
-                  </Button>
-                  <span>
-                    Expected Attendies 40</span>
+                  <div className={classes.timeDetail}>
+                    <span className={classes.timeDetailText}>Started At </span>
+                    <span className={classes.timeDetailHour}>{moment(details.start_hour).utcOffset(1, true).format('hh:mm')}</span>
                   </div>
+                  {(details.end_hour!=null)&&(
+                    <div className={classes.timeDetail}>
+                      <span className={classes.timeDetailText}>Ended At </span>
+                      <span className={classes.timeDetailHour}>{moment(details.end_hour).utcOffset(1, true).format('hh:mm')}</span>
+                  </div>)}
                 </div>
-          </div>
+                  <div  style={containers.containerDetail}>
+                    <Button fab disabled>
+                      <People color="action"/>
+                    </Button>
+                    <div>
+                      <span>
+                        Expected Attendies 40
+                      </span>
+                    </div>
+                  </div>
+              </div>
         </div>)
       }
   }
