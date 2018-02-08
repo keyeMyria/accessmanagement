deb1833f389f8f9e13512d7a372fbe4733f65fe6
Hallly <halasouid@gmail.com>
2018-02-08 10:02:41 +0100

    Bottom nav icons

--- src/client/components/App/BottomBarComponent.js
+++ src/client/components/App/BottomBarComponent.js
@@ -9,7 +9,8 @@ import LocationOnIcon from 'material-ui-icons/LocationOn';
 import People from 'material-ui-icons/People';
 import PhotoCamera from 'material-ui-icons/PhotoCamera';
 import SwapHoriz from 'material-ui-icons/SwapHoriz';
-import DonutSmall from 'material-ui-icons/DonutSmall';
+import DonutLarge from 'material-ui-icons/DonutLarge';
+import GroupWork from 'material-ui-icons/GroupWork';
 import TransferWithinAStation from 'material-ui-icons/TransferWithinAStation'
 import Toys from 'material-ui-icons/Toys';
 import { withRouter } from 'react-router';
@@ -35,12 +36,12 @@ class BottomBarComponent extends React.Component{
         >
         {(role=='agent_in' || role=='agent_out'|| role=='agent_workshop')&&(<BottomNavigationAction label="حالة الحضور" icon={<People />} value="/listattendies" />)}
         {(role=='agent_in' || role=='agent_out'|| role=='agent_workshop')&&(<BottomNavigationAction label="التقاط الرمز" icon={<PhotoCamera />} value="/agent"/>)}
-        {(role=='agent_in' || role=='agent_out'|| role=='agent_workshop')&&(<BottomNavigationAction label="Dashboard" icon={<DonutSmall />} value="/dashboard"/>)}
+        {(role=='agent_in' || role=='agent_out'|| role=='agent_workshop')&&(<BottomNavigationAction label="لوحة المراقبة" icon={<DonutSmall />} value="/dashboard"/>)}
         {(role=='agent_in' || role=='agent_out'|| role=='agent_workshop')&&(<BottomNavigationAction label="Operate on list" icon={<TransferWithinAStation />} value="/operateonguestlist"/>)}
         {((role=='admin')&&(this.props.addEventItems==true))&&(<BottomNavigationAction label="حالة الحضور" icon={<People />} value="/listguests" />)}
         {((role=='admin')&&(this.props.addEventItems==true))&&(<BottomNavigationAction label="التحركات" icon={<SwapHoriz />} value="/activitylog"/>)}
-        {((role=='admin') &&(this.props.addEventItems==true))&&(<BottomNavigationAction label="Dashboard" icon={<DonutSmall />} value={`/event-dashboard/${this.props.eventid}`}/>)}
-        {((role=='admin')&&(this.props.addEventItems==true))&&(<BottomNavigationAction label="الجلسات" icon={<Toys />} value="/activitylog"/>)}
+        {((role=='admin') &&(this.props.addEventItems==true))&&(<BottomNavigationAction label="لوحة المراقبة" icon={<DonutLarge />} value={`/event-dashboard/${this.props.eventid}`}/>)}
+        {((role=='admin')&&(this.props.addEventItems==true))&&(<BottomNavigationAction label="الجلسات" icon={<GroupWork />} value="/activitylog"/>)}
       </BottomNavigation>
     )
     }
